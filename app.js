02aba7e7474a75038f31d36952b5cd4f06b664aa
SantoshRauniyar/ketu_the_scrapper <46098076+SantoshRauniyar@users.noreply.github.com>
2021-06-10 23:41:26 +0530

    Access Data members

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 new Vue({
     el:'#vue-app',
     data:{
-        name:'Santosh Rauniyar',
+        name:'Santosh Rauniyar',//data members
         roll:'20408'
     },
     methods:{
         greet:function(time){
-            return "Good "+time;
+            return "Good "+ time +'  '+ this.name;
         }
     }
 });

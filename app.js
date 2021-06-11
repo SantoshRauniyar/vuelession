c04df67f93832c53b7417645348bf378d254b949
SantoshRauniyar/ketu_the_scrapper <46098076+SantoshRauniyar@users.noreply.github.com>
2021-06-11 10:39:23 +0530

    event changes

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 new Vue({
+    el:'#vue-app',
+    data:{
+        name:'Santosh Rauniyar',//data members
+        roll:'20408'
+    },
+    methods:{
+        greet:function(time){
+            return "Good "+ time +'  '+ this.name;
+        }
+    }
+});
+
+new Vue({
     el:'#event-handle',
     data:{
         age:10
